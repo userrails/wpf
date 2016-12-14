d9655c84f40b813314f28adf65ac0aa211829635
Shiv Raj Badu <shivrajbadu@gmail.com>
2016-12-14 23:31:01 +0545

    Fixed some typecasting and reference not set to an object, etc

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -171,17 +171,21 @@ namespace WpfApp
         // ListViewItem_DoubleClick
         private void ListViewItem_DoubleClick(object sender, RoutedEventArgs e)
         {
-           System.Data.DataRowView cusObj = (System.Data.DataRowView)lvCus.SelectedItem;
-           var myobj = cusObj.Row.ItemArray;
-           txtCusID.Text = myobj[0].ToString();    
-           txtfn.Text = myobj[1].ToString();
-           txtln.Text = myobj[2].ToString();
-           txtdob.Text = myobj[3].ToString();
-           txtage.Text = myobj[4].ToString();
+           System.Data.DataRowView cusObj = lvCus.SelectedItem as System.Data.DataRowView;
+           
+           if (cusObj != null)
+           {
+               var myobj = cusObj.Row.ItemArray;
+               txtCusID.Text = myobj[0].ToString();
+               txtfn.Text = myobj[1].ToString();
+               txtln.Text = myobj[2].ToString();
+               txtdob.Text = myobj[3].ToString();
+               txtage.Text = myobj[4].ToString();
 
-           btnUpdate.Visibility = System.Windows.Visibility.Visible;
-           btnDelete.Visibility = System.Windows.Visibility.Visible;
-           btnSave.Visibility = System.Windows.Visibility.Hidden;
+               btnUpdate.Visibility = System.Windows.Visibility.Visible;
+               btnDelete.Visibility = System.Windows.Visibility.Visible;
+               btnSave.Visibility = System.Windows.Visibility.Hidden;
+           }
         }
         // ListViewItem_DoubleClick
 
